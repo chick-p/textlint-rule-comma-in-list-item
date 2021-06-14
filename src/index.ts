70ac10abadeaaeed53f59076dc2c8a3bfd537b04
chick-p <chick.p.1120@gmail.com>
2021-06-15 06:26:34 +0900

    feat: implement textlint-rule-comma-in-list-item

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,62 @@
 import { TextlintRuleModule } from "@textlint/types";
+import { TxtNode } from "@textlint/ast-node-types";
+import execAll from "execall";
 
 export interface Options {
-    // if the Str includes `allows` word, does not report it
-    allows?: string[];
+  commmaMarks: string[];
+  allowOrderedList: boolean;
 }
 
-const report: TextlintRuleModule<Options> = (context, options = {}) => {
-    const {Syntax, RuleError, report, getSource} = context;
-    const allows = options.allows || [];
-    return {
-        [Syntax.Str](node) { // "Str" node
-            const text = getSource(node); // Get text
-            const matches = /bugs/g.exec(text); // Found "bugs"
-            if (!matches) {
-                return;
-            }
-            const isIgnored = allows.some(allow => text.includes(allow));
-            if (isIgnored) {
-                return;
-            }
-            const indexOfBugs = matches.index;
-            const ruleError = new RuleError("Found bugs.", {
-                index: indexOfBugs // padding of index
-            });
-            report(node, ruleError);
-        }
-    }
+const isItemNodeInOrderedList = (node: TxtNode) => {
+  return node && node.parent && node.parent.ordered === true;
 };
-export default report;
+
+const defaultOptions = {
+  commmaMarks: [",", "、", "，"],
+  allowOrderedList: false,
+};
+
+const reporter: TextlintRuleModule<Partial<Options>> = (
+  context,
+  options = {}
+) => {
+  const { Syntax, RuleError, report, getSource } = context;
+
+  const commmaMarks =
+    options.commmaMarks !== undefined
+      ? options.commmaMarks
+      : defaultOptions.commmaMarks;
+  const allowOrderedList =
+    options.allowOrderedList !== undefined
+      ? options.allowOrderedList
+      : defaultOptions.allowOrderedList;
+
+  const commaPattern = new RegExp(`\[${commmaMarks.join("")}\]`, "g");
+  return {
+    [Syntax.ListItem](node) {
+      // Skip OrderddList if option is exabled.
+      if (allowOrderedList && isItemNodeInOrderedList(node)) {
+        return;
+      }
+      // A ListItem should includes child nodes.
+      // https://github.com/textlint-rule/textlint-rule-period-in-list-item/issues/3
+      const paragraphNodes = node.children.filter(
+        (node) => node.type === Syntax.Paragraph
+      );
+      const firstParagraphNode = paragraphNodes[0];
+      if (!firstParagraphNode) {
+        return;
+      }
+      const text = getSource(firstParagraphNode);
+      const results = execAll(commaPattern, text);
+      results.forEach((result) => {
+        const index = result.index;
+        const ruleError = new RuleError(`Include comma.`, {
+          index: index,
+        });
+        report(node, ruleError);
+      });
+    },
+  };
+};
+export default reporter;
